1dcfeddd37aedde77e6259dd43fac17cd0632e20
Vlad Kolotov <vkolotov@gmail.com>
2017-09-16 23:38:50 +1200

    Bugfixes from travis release process Signed-off-by: Vlad Kolotov <vkolotov@gmail.com>

--- .travis/before-deploy.sh
+++ .travis/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+if [[ "$TRAVIS_BRANCH" == release* ]] && [[ "$TRAVIS_PULL_REQUEST" == 'false' ]] && [[ "$TRAVIS_TAG" == release* ]] ; then
     openssl aes-256-cbc -K $encrypted_05df9d2256fa_key -iv $encrypted_05df9d2256fa_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
     gpg --fast-import .travis/codesigning.asc
 fi
--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ] && [ ! -z "$TRAVIS_TAG" ]; then
+if [[ "$TRAVIS_BRANCH" == release* ]] && [[ "$TRAVIS_PULL_REQUEST" == 'false' ]] && [[ "$TRAVIS_TAG" == release* ]] ; then
     #mvn deploy -P sign,build-extras --settings .travis/settings.xml
     mvn release:prepare -B -P sign,build-extras --settings .travis/settings.xml
     mvn release:perform -B -P sign,build-extras --settings .travis/settings.xml
