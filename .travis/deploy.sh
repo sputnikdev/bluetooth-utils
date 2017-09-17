6eb9c4bea73b80f22bea4c6393ea4401998926e2
Vlad Kolotov <vkolotov@gmail.com>
2017-09-17 19:29:39 +1200

    extracting release version from tags Signed-off-by: Vlad Kolotov <vkolotov@gmail.com>

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-ARTIFACT_NAME=$( echo "$TRAVIS_REPO_SLUG" | sed 's:.*/::' )
+VERSION=$( echo "$TRAVIS_TAG" | sed 's:.*-::' )
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ] && [ ! -z "$TRAVIS_TAG" ]
 then
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
-    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$ARTIFACT_NAME-$TRAVIS_TAG 1>/dev/null 2>/dev/null
+    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$VERSION 1>/dev/null 2>/dev/null
     mvn deploy -P sign,build-extras --settings .travis/settings.xml
 fi
